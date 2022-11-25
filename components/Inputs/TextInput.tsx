50928dd54e916a4a57fdef36a1f41e643097f0cd
Marcelina Sitko <xmarcelasx@gmail.com>
2022-11-25 21:42:02 +0100

    create text input

---
+++ components/Inputs/TextInput.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import { InputProps } from 'types/components/Inputs/types';
+import { Input } from './Input';
+
+interface Props extends InputProps {
+    keyboardType?: 'email-address' | 'url' | 'default';
+}
+
+export const TextInput: FC<Props> = (props) => {
+    return (
+        <Input
+            keyboardType={props.keyboardType}
+            {...props}
+        />
+    );
+};
