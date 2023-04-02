fc0af932cafa3ed39e21993012416257415fe817
Marcelina Sitko <xmarcelasx@gmail.com>
2023-04-02 23:01:46 +0200

    create WideButton component

---
+++ components/Buttons/WideButton.tsx
@@ -0,0 +1,45 @@
+import { Ionicons } from "@expo/vector-icons";
+import { BreakLine } from "components/Composition/BreakLine";
+import { FC } from "react";
+import { GestureResponderEvent, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import colors from "themes/colors";
+import icons from "themes/icons";
+
+interface Props {
+    onPress: (event: GestureResponderEvent) => void;
+    title: string;
+    icon?: any;
+}
+
+export const WideButton: FC<Props> = ({ onPress, title, icon }) => {
+    return (
+        <TouchableWithoutFeedback onPress={onPress}>
+            <View>
+                <View style={styles.container}>
+                    {icon && <Ionicons name={icon} style={styles.leftIcon} size={26} color={colors.BLACK} />}
+                    <Text style={styles.title}>{title}</Text>
+                    <Ionicons name={icons.CHEVRON_FORWARD_OUTLINE} style={styles.rightIcon} size={24} color={colors.GRAY_DARK} />
+                </View>
+                <BreakLine />
+            </View>
+        </TouchableWithoutFeedback>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 14
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '500'
+    },
+    leftIcon: {
+        marginRight: 20
+    },
+    rightIcon: {
+        marginLeft: 'auto'
+    }
+});
