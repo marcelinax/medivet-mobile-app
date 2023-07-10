cc2d0d8f4198a05f022f7088c8deb2298c395e5b
Marcelina Sitko <xmarcelasx@gmail.com>
2023-07-10 22:07:33 +0200

    configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,59 +1,65 @@
 module.exports = {
-    env: { es2021: true },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-      ],
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['react'],
-    rules: {
-        'no-undef': 'error',
-        'func-names': 'off',
-        'react/function-component-definition': 'off',
-        'array-element-newline': [
-        'error', {
-            ArrayExpression: 'consistent',
-            ArrayPattern: { minItems: 2 },
-        },
-        ],
-        'object-property-newline': [
-        'error', {
-            allowMultiplePropertiesPerLine: false,
-        },
-        ],
-        'object-curly-newline': ['error'],
-        'no-multi-spaces': 'error',
-        'no-irregular-whitespace': 'error',
-        'no-trailing-spaces': 'error',
-        'import/no-extraneous-dependencies': 'off',
-        'global-require': 'off',
-        'max-len': [
-        'error', {
-            code: 140,
-            tabWidth: 2,
-        },
-        ],
-        indent: ['error', 2],
-        'no-tabs': 'error',
-        semi: ['error'],
-        'linebreak-style': 'off',
-        'import/no-unresolved': 'off',
-        'react/prop-types': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-filename-extension': 'off',
-        'no-lone-blocks': 'error',
-        'eol-last': 'error',
-        'react/jsx-max-props-per-line': ['error', { maximum: 1 }],
-        'no-use-before-define': 'off',
-        'react/style-prop-object': 'off',
-        'no-unused-vars': 'warn',
-        'react/jsx-indent': 'off',
-        'consistent-return': 'off',
-        'import/prefer-default-export': 'off',
-        'array-bracket-newline': ['error', { multiline: true }]
-    }
-}
+  env: {es2021: true},
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    '@typescript-eslint/no-undef': 'error',
+    'func-names': 'off',
+    'array-element-newline': [
+      'error',
+      {
+        ArrayExpression: 'consistent',
+        ArrayPattern: {minItems: 2},
+      },
+    ],
+    'object-property-newline': [
+      'error',
+      {
+        allowMultiplePropertiesPerLine: false,
+      },
+    ],
+    'object-curly-newline': ['error'],
+    'no-multi-spaces': 'error',
+    'no-irregular-whitespace': 'error',
+    'no-trailing-spaces': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'global-require': 'off',
+    'max-len': [
+      'error',
+      {
+        code: 140,
+        tabWidth: 2,
+      },
+    ],
+    indent: ['error', 2],
+    'no-tabs': 'error',
+    semi: ['error'],
+    'linebreak-style': 'off',
+    'import/no-unresolved': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'no-lone-blocks': 'error',
+    'eol-last': 'error',
+    'react/jsx-max-props-per-line': ['error', {maximum: 1}],
+    'no-use-before-define': 'off',
+    'react/style-prop-object': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/jsx-indent': 'off',
+    'consistent-return': 'off',
+    'import/prefer-default-export': 'off',
+    'array-bracket-newline': ['error', {multiline: true}],
+    '@typescript-eslint/no-implicit-any-catch': 'error',
+    'react/function-component-definition': 'off',
+    'react/require-default-props': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  },
+};
