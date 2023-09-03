35b2bed2e4c7e85c450bd33e66350050621adaf6
Marcelina Sitko <xmarcelasx@gmail.com>
2023-09-03 23:10:57 +0200

    possibility to search by array keys in SimpleList

--- components/List/SimpleList.tsx
+++ components/List/SimpleList.tsx
@@ -1,8 +1,12 @@
-import { FlatList, ListRenderItem, View } from 'react-native';
-import { listStyles } from 'components/List/utils/styles';
+import {FlatList, ListRenderItem, View} from 'react-native';
+import {listStyles} from 'components/List/utils/styles';
 import colors from 'themes/colors';
-import { EmptyList } from 'components/Composition/EmptyList';
-import { LoadingButton } from 'components/Buttons/LoadingButton';
+import {EmptyList} from 'components/Composition/EmptyList';
+import {LoadingButton} from 'components/Buttons/LoadingButton';
+import {useEffect, useState} from "react";
+import {Loading} from "components/Composition/Loading";
+import {TextInput} from "components/Inputs/TextInput";
+import {inputsTranslations} from "constants/translations/inputs.translations";
 
 interface Props {
   data: any[];
@@ -11,33 +15,83 @@ interface Props {
   stickyFooterButtonTitle?: string;
   stickyFooterButtonAction?: () => void;
   stickyButtonLoading?: boolean;
+  searchKeys?: string[];
 }
 
-// dorobić wyszukiwanie tej liście jako opcjonalne
 export const SimpleList = ({
-  data,
-  renderItem,
-  separateOptions,
-  stickyButtonLoading,
-  stickyFooterButtonTitle,
-  stickyFooterButtonAction,
-}: Props) => {
-  const itemSeparator = separateOptions ? () => <View style={listStyles.separator} /> : undefined;
+                             data,
+                             renderItem,
+                             separateOptions,
+                             stickyButtonLoading,
+                             stickyFooterButtonTitle,
+                             stickyFooterButtonAction,
+                             searchKeys
+                           }: Props) => {
+  const [search, setSearch] = useState<string>('');
+  const [loading, setLoading] = useState(false);
+  const withSearch = searchKeys && searchKeys.length > 0;
+  const [internalData, setInternalData] = useState<any[]>([...data]);
 
-  const emptyComponent: JSX.Element = data.length === 0 ? <EmptyList /> : <></>;
+  const itemSeparator = separateOptions ? () => <View style={listStyles.separator}/> : undefined;
+  const emptyComponent: JSX.Element = !loading && internalData.length === 0 ? <EmptyList/> : <></>;
+  const footerComponent: JSX.Element = loading ? <Loading/> : <></>;
+
+  useEffect(() => {
+    setLoading(true);
+    const searchTimeout = setTimeout(() => {
+      handleSearchData();
+    }, 300);
+
+    return () => clearTimeout(searchTimeout);
+  }, [search]);
+
+  const handleSearchData = () => {
+    if (!withSearch) return internalData;
+
+    if (!search) setInternalData([...data]);
+    else {
+      const filteredData = data.filter(item => {
+        if (searchKeys!.some(key => item[key]?.toLowerCase()?.includes(search.toLowerCase()))) return item;
+      })
+
+      setInternalData([...filteredData]);
+    }
+    setLoading(false);
+  }
+
+  const handleChangeSearch = (value: string): void => {
+    setSearch(value);
+    setInternalData([])
+  };
+
+  const headerComponent: JSX.Element = (
+    <View style={listStyles.inputContainer}>
+      <TextInput
+        variant="outline"
+        value={search}
+        onChangeText={handleChangeSearch}
+        errors={[]}
+        autoCapitalize="none"
+        placeholder={inputsTranslations.SEARCH}
+      />
+    </View>
+  );
 
   return (
-    <View style={[ listStyles.container, { backgroundColor: colors.WHITE } ]}>
+    <View style={[listStyles.container, {backgroundColor: colors.WHITE}]}>
       <FlatList
-        data={data}
+        data={internalData}
         renderItem={renderItem}
         bounces={false}
         ItemSeparatorComponent={itemSeparator}
         keyExtractor={(item) => item.id}
         ListEmptyComponent={emptyComponent}
+        ListFooterComponent={footerComponent}
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ flexGrow: 1 }}
+        contentContainerStyle={{flexGrow: 1}}
         style={listStyles.list}
+        stickyHeaderIndices={withSearch ? [0] : undefined}
+        ListHeaderComponent={withSearch ? headerComponent : <></>}
       />
       {
         stickyFooterButtonTitle && stickyFooterButtonAction && (
