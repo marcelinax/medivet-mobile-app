15ac83e08d7895e5a8781b9786a039532c8d61ef
Marcelina Sitko <xmarcelasx@gmail.com>
2023-12-22 22:38:42 +0100

    add "make an appointment" button in vet preview

--- components/Screens/Home/Vet/VetInfo.tsx
+++ components/Screens/Home/Vet/VetInfo.tsx
@@ -4,6 +4,10 @@ import { Avatar } from 'components/Composition/Avatar';
 import { OpinionRating } from 'components/Composition/OpinionRating';
 import colors from 'themes/colors';
 import { VetOpinion } from 'types/api/opinion/types';
+import { Button } from 'components/Buttons/Button';
+import { useTranslation } from 'react-i18next';
+import { useNavigation } from '@react-navigation/native';
+import { NavigationProps } from 'types/Navigation/types';
 
 interface Props {
   vet: User;
@@ -13,19 +17,36 @@ interface Props {
 
 export const VetInfo = ({ vet, opinions, opinionsAmount }: Props) => {
   const specializations = (vet.specializations || []).map((specialization) => specialization.name).join(', ');
+  const { t } = useTranslation();
+  const navigation = useNavigation<NavigationProps>();
 
   return (
-    <View style={styles.row}>
-      <Avatar
-        size="large"
-        url={vet.profilePhotoUrl}
-      />
-      <View style={styles.infoContainer}>
-        <Text style={styles.name}>{vet.name}</Text>
-        <Text style={styles.specializations}>{specializations}</Text>
-        <OpinionRating
-          opinions={opinions}
-          opinionsAmount={opinionsAmount}
+    <View>
+      <View style={styles.row}>
+        <Avatar
+          size="large"
+          url={vet.profilePhotoUrl}
+        />
+        <View style={styles.infoContainer}>
+          <Text style={styles.name}>{vet.name}</Text>
+          <Text style={styles.specializations}>{specializations}</Text>
+          <OpinionRating
+            opinions={opinions}
+            opinionsAmount={opinionsAmount}
+          />
+        </View>
+      </View>
+      <View style={{
+        marginTop: 16,
+        marginBottom: 16,
+      }}
+      >
+        <Button
+          title={t('actions.make_an_appointment.title')}
+          variant="solid"
+          onPress={() => navigation.navigate('Appointment Calendar', {
+            vet,
+          })}
         />
       </View>
     </View>
