8c0fd322851b0bec148c0ebecf8ddc76ad69102a
Marcelina Sitko <xmarcelasx@gmail.com>
2024-03-03 19:48:55 +0100

    fetch only active animals during appointment creation

--- components/Screens/Home/Appointment/AnimalList.tsx
+++ components/Screens/Home/Appointment/AnimalList.tsx
@@ -3,13 +3,17 @@ import { Animal } from 'types/api/animal/types';
 import { List } from 'components/List/List';
 import { AnimalApi } from 'api/animal/animal.api';
 import { AnimalListItem } from 'components/Screens/Home/Appointment/AnimalListItem';
+import { AnimalStatus } from 'constants/enums/enums';
 
 export const AnimalList = () => {
   const renderAnimal: ListRenderItem<Animal> = ({ item }) => <AnimalListItem animal={item} />;
 
   return (
     <List
-      onFetch={AnimalApi.getOwnerAnimals}
+      onFetch={(params) => AnimalApi.getOwnerAnimals({
+        ...params,
+        status: AnimalStatus.ACTIVE,
+      })}
       renderItem={renderAnimal}
       separateOptions
       withoutBackgroundColor
